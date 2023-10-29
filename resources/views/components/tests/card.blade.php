f179f7935b2460e8718a6287c7ec479e3b172279
Akito mikami <akitomikami@outlook.jp>
2023-10-30 01:39:16 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+    "title" => "タイトルなし",
+    "message" => "初期値です",
+    "content" => "初期",
+])
+
 <div class="border-2 shadow-md w-1/4 p-2">
     <div>{{ $title }}</div>
     <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,4 +2,5 @@
     <x-slot name="header">ヘッダー1</x-slot>
     1
     <x-tests.card title="タイトル" content="コンテント" :message="$message" />
+    <x-tests.card title="タイトル" />
 </x-tests.app>
