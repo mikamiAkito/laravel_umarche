36342bfa9a507c1eda360e4bddefe0c183f3fef4
Akito mikami <akitomikami@outlook.jp>
2023-10-30 01:48:38 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
     1
     <x-tests.card title="タイトル" content="コンテント" :message="$message" />
     <x-tests.card title="タイトル" />
+    <x-tests.card title="CSSを変更" class="bg-red-300" />
 </x-tests.app>
