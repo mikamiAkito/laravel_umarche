da79dd5381e7d7140edb2592e9a0acee4b9cc573
Akito mikami <akitomikami@outlook.jp>
2023-11-23 01:46:03 +0900

    micromodal

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -27,14 +27,36 @@
                                     </select>
                                 </div>
                             </div>
+                            <x-select-image :images="$images" name="image1" />
+                            <x-select-image :images="$images" name="image2" />
+                            <x-select-image :images="$images" name="image3" />
+                            <x-select-image :images="$images" name="image4" />
                             <div class="p-2 w-full flex justify-center space-x-8 mt-4">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index') }}'" class="bg-gray-100 border-0 py-2 px-8 focus:outline-none hover:bg-gray-700 rounded text-lg">戻る</button>
                                 <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録</button>
                             </div>
                         </div>
-                    </form>
+                    </form>                    
                 </div>
             </div>
         </div>
     </div>
+    <script>
+        'use strict'
+        const images = document.querySelectorAll('.image')//すべてのimageタグを取得
+        images.forEach( image => {//１つずつ繰り返す
+            image.addEventListener('click', function(e){//クリックしたら
+                const imageName = e.target.dataset.id.substr(0,6)//data-idの6文字
+                const imageId = e.target.dataset.id.replace(imageName + '_', '')//６文字カット
+                const imageFile = e.target.dataset.file
+                const imagePath = e.target.dataset.path
+                const modal = e.target.dataset.modal
+
+                //サムネイルとinput type=hiddenのvalueに設定
+                document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
+                document.getElementById(imageName + '_hidden').value = imageId
+                MicroModal.close(modal);//モーダルを閉じる
+        })
+    })    
+    </script>
 </x-app-layout>
